357295b238fe41c812a33c0f62836fa5ffca0ba5
dolf <kolyakutylov@gmail.com>
2022-09-28 19:56:50 +0700

    redo to countdown timer

---
+++ script/script.js
@@ -0,0 +1,21 @@
+let hours = document.querySelector('.hours')
+let minutes = document.querySelector('.minutes')
+let seconds = document.querySelector('.seconds')
+let days = document.querySelector('.days')
+
+const s = 1000,
+    m = s * 60,
+    h = m * 60,
+    d = h * 24;
+
+setInterval(() => {
+    const countDown = new Date(2023, 0, 1).getTime();
+    const currentTime = new Date().getTime();
+    let distance = countDown - currentTime;
+
+    days.textContent = Math.floor(distance / d)
+    hours.textContent = Math.floor((distance % d) / h)
+    minutes.textContent = Math.floor((distance % h) / m)
+    seconds.textContent = Math.floor((distance % m) / s)
+
+}, 1000)
